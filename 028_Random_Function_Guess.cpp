5bb2205759fbfb0c83b6816fbd1d488f05250b35
abdullah-abwisk <aabdullahkhan4@gmail.com>
2021-01-27 22:44:57 -0800

    C++

--- 028_Random_Function_Guess.cpp
+++ 028_Random_Function_Guess.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include <cstdlib>
+#include <time.h>
 
 int main()
 {
@@ -9,6 +11,7 @@ int main()
     cin >> loop;
     while (loop == 'y')
     {
+    	srand(time(0));
         generated_number = (rand() % 100) + 1;
         cout << "Enter your guess: ";
         cin >> guess_number;
